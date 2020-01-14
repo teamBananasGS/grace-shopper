f5fc9bd319ee3fc263b020a3ed0f703a41e5737b
Henry Chong <chong.henry@gmail.com>
2020-01-14 14:21:23 -0500

    shopping cart model updates

--- server/db/models/shoppingCart.js
+++ server/db/models/shoppingCart.js
@@ -2,28 +2,17 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const ShoppingCart = db.define('shoppingcart', {
+  shoppingCartId: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
   userId: {
     type: Sequelize.INTEGER
   },
-  itemId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  itemQuantity: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0
-  },
-  itemPrice: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  totalPrice: {
-    type: Sequelize.INTEGER,
-    defaultValue: 0
-  },
-  imageUrl: {
-    type: Sequelize.TEXT,
-    defaultValue: 'http://tny.im/kqt'
+  isGuest: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
   }
 })
 
