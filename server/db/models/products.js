5937ac9d185c7e5f79d4d4a04fd54338813bccd0
Remi Mendoza <mendozaremi@gmail.com>
2020-01-14 11:55:38 -0500

    chore: add products.js model

---
+++ server/db/models/products.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Products = db.define('products', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  category: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isIn: [['Suit', 'Watch', 'Shoe']]
+    }
+  },
+  stock: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    defaultValue: 'http://tny.im/kqt'
+  }
+})
+
+module.exports = Products
